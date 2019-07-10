7c4993ac25056dda515377ce96235a128c73106f
tab-zhou <31908447+tab-zhou@users.noreply.github.com>
2019-07-10 18:51:46 +0800

    add app config update

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from app import app
 from app.user import user
 from app.test import test
 
+app.config.update(DEBUG=True)
+app.config.update(ENV="development")
+
 app.register_blueprint(user, url_prefix='/user')
 app.register_blueprint(test, url_prefix='/test')
 
